c644b1e83a80068b528d5187347e092693de4818
Andrei Ulyanov <ulya0003@algonquinlive.com>
2021-03-23 13:53:55 -0400

    cursor01.py completed

--- cursor01.py
+++ cursor01.py
@@ -1,2 +1,20 @@
-import sys
+import arcpy
+
+wsp = r'..\..\data\Canada'
+Maj_City_fc = r'..\..\data\Canada\Can_Mjr_Cities.shp'
+arcpy.env.workspace = wsp
+
+fclist = arcpy.ListFeatureClasses()
+
+print(fclist)
+
+#with arcpy.da.SearchCursor(fc, 'NAME', 'PROV')as cursor:
+cursor = arcpy.SearchCursor(Maj_City_fc)
+count = 0
+print('Name, Prov')
+for row in cursor:
+    print(row.getValue('NAME')+ ',' + row.getValue('PROV'))
+    count += 1
+
+print(count)
 
