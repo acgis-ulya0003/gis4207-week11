6c33bf924e9786d65af8c6bbf5203f8a0ac4ee24
Andrei Ulyanov <ulya0003@algonquinlive.com>
2021-03-23 15:06:10 -0400

    cursor02.py completed

--- cursor01.py
+++ cursor01.py
@@ -4,17 +4,19 @@ wsp = r'..\..\data\Canada'
 Maj_City_fc = r'..\..\data\Canada\Can_Mjr_Cities.shp'
 arcpy.env.workspace = wsp
 
-fclist = arcpy.ListFeatureClasses()
 
-print(fclist)
 
-#with arcpy.da.SearchCursor(fc, 'NAME', 'PROV')as cursor:
-cursor = arcpy.SearchCursor(Maj_City_fc)
+fields = ['NAME', 'PROV']
+cursor = arcpy.da.SearchCursor(Maj_City_fc, fields)
+
 count = 0
 print('Name, Prov')
 for row in cursor:
-    print(row.getValue('NAME')+ ',' + row.getValue('PROV'))
+    
+    print(str(row).strip('(\'').strip('\')'))
     count += 1
 
-print(count)
+count = str(count)
+print('\n')
+print('There are ' + count + ' cities in the above list.')
 
---
+++ cursor02.py
@@ -0,0 +1,40 @@
+import sys
+
+def main():
+
+    if len(sys.argv) != 2:
+        print('Usage: cursor02.py <province name>')
+    else:
+        import arcpy
+
+        wsp = r'..\..\data\Canada'
+        Maj_City_fc = r'..\..\data\Canada\Can_Mjr_Cities.shp'
+        arcpy.env.workspace = wsp
+
+        field = ['NAME', 'PROV']
+
+        count = 0
+
+        print('Name')
+
+        prov_code = sys.argv[1].upper()
+
+        search_fields = arcpy.AddFieldDelimiters(wsp, 'PROV')
+        where_clause = f"{search_fields} = '{prov_code}'"
+
+        cursor = arcpy.da.SearchCursor(Maj_City_fc, field, where_clause)
+
+        for row in cursor:
+            #print(row)
+            print(str(row).strip('(\'').strip('\')'))
+            #print(str(row).strip('(\'').strip('\')'))
+            count += 1
+
+        count = str(count)
+        print('\n')
+        print('There are ' + count + ' cities in the above list.')
+
+if __name__ == '__main__':
+    main()
+
+#python cursor02.py on
